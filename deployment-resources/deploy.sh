c3dcbd462e4419396d0898f93a461d5168f77896
Tim Summerton-Brier <t.brier93@gmail.com>
2023-10-30 20:49:10 +0000

    Add deployment script

---
+++ deployment-resources/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install java if required
+if [ ! -d "jdk-17-ga" ]; then
+  echo "Installing raspberry pi 1 compatible jdk..."
+  wget https://github.com/sizlo/raspberry-pi-spring-boot-spike/raw/main/deployment-resources/jdk-17-ga_gcc-10.1_glibc-2.28_binutils-2.31_Buster.tar.gz
+  tar zxf jdk-17-ga_gcc-10.1_glibc-2.28_binutils-2.31_Buster.tar.gz
+  echo "jdk installed"
+fi
+
+echo "Downloading latest jar"
+RELEASE_JSON="$(curl https://api.github.com/repos/sizlo/raspberry-pi-spring-boot-spike/releases/latest)"
+FILENAME="$(echo $RELEASE_JSON | jq -r '.assets[0].name')"
+URL="$(echo $RELEASE_JSON | jq -r '.assets[0].browser_download_url')"
+wget $URL
+echo "jar downloaded"
+
+echo "Running jar"
+jdk-17-ga/bin/java -jar "$FILENAME"

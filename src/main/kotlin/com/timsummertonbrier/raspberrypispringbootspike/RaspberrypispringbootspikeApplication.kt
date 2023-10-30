fca3792361f7f0d2e6d328512ee1bf243df25379
Tim Summerton-Brier <t.brier93@gmail.com>
2023-10-30 19:55:31 +0000

    Initial commit of project from spring initializr

---
+++ src/main/kotlin/com/timsummertonbrier/raspberrypispringbootspike/RaspberrypispringbootspikeApplication.kt
@@ -0,0 +1,11 @@
+package com.timsummertonbrier.raspberrypispringbootspike
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RaspberrypispringbootspikeApplication
+
+fun main(args: Array<String>) {
+	runApplication<RaspberrypispringbootspikeApplication>(*args)
+}
---
+++ src/test/kotlin/com/timsummertonbrier/raspberrypispringbootspike/RaspberrypispringbootspikeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.timsummertonbrier.raspberrypispringbootspike
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RaspberrypispringbootspikeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

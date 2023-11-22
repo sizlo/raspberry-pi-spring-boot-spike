403e72d43c418cfa86fab09935c6ed7d7fadae03
Tim Summerton-Brier <t.brier93@gmail.com>
2023-11-22 09:06:34 +0000

    Display errors in form

--- src/main/kotlin/com/timsummertonbrier/raspberrypispringbootspike/Books.kt
+++ src/main/kotlin/com/timsummertonbrier/raspberrypispringbootspike/Books.kt
@@ -1,13 +1,21 @@
 package com.timsummertonbrier.raspberrypispringbootspike
 
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
 import org.springframework.stereotype.Controller
 import org.springframework.stereotype.Repository
 import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
-data class Book(val title: String, val category: String)
+data class Book(
+    @field:NotBlank
+    val title: String,
+    @field:NotBlank
+    val category: String,
+)
 data class NewBook(val title: String? = null, val category: String? = null)
 
 @Repository
@@ -37,7 +45,11 @@ class BookController(private val bookRepository: BookRepository) {
     }
 
     @PostMapping("/add")
-    fun addBook(book: Book): String {
+    fun addBook(@Valid book: Book, bindingResult: BindingResult): String {
+        if (bindingResult.hasErrors()) {
+            return "add-book"
+        }
+
         bookRepository.addBook(book)
         return "redirect:/books"
     }
